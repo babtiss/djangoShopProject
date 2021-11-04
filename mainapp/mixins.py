532c8d65d4cd383a0c3e71c357adba6f5941bc47
babtiss <bigvovasuhov@mail.ru>
2021-11-04 18:39:21 +0300

    refactoring code - created a new func for mixins

--- mainapp/mixins.py
+++ mainapp/mixins.py
@@ -3,36 +3,30 @@ from django.views.generic import View
 from mainapp.models import *
 
 
+def FindCart(request):
+    cart = None
+    customer = None
+    if request.user.is_authenticated:
+        customer = Customer.objects.filter(user=request.user).first()
+        if not customer:
+            customer = Customer.objects.create(user=request.user)
+        cart = Cart.objects.filter(owner=customer, in_order=False).first()
+        if not cart:
+            cart = Cart.objects.create(owner=customer, final_price=0, number_of_products=0)
+    return cart, customer
+
+
 class CustomerAndCartMixin(View):
 
     def dispatch(self, request, *args, **kwargs):
-        cart = None
-        customer = None
-        if request.user.is_authenticated:
-            customer = Customer.objects.filter(user=request.user).first()
-            if not customer:
-                customer = Customer.objects.create(user=request.user)
-            cart = Cart.objects.filter(owner=customer, in_order=False).first()
-            if not cart:
-                cart = Cart.objects.create(owner=customer, final_price=0, number_of_products=0)
-        self.cart = cart
-        self.customer = customer
+        self.cart, self.customer = FindCart(request)
         self.categories = Category.objects.all()
         return super().dispatch(request, *args, **kwargs)
 
 
 class CartProductMixin(View):
     def dispatch(self, request, *args, **kwargs):
-        cart = None
-        customer = None
-        if request.user.is_authenticated:
-            customer = Customer.objects.filter(user=request.user).first()
-            if not customer:
-                customer = Customer.objects.create(user=request.user)
-            cart = Cart.objects.filter(owner=customer, in_order=False).first()
-            if not cart:
-                cart = Cart.objects.create(owner=customer, final_price=0, number_of_products=0)
-        self.cart = cart
+        self.cart, self.customer = FindCart(request)
         self.slug = kwargs.get('slug')
         product = Product.objects.get(slug=self.slug)
         self.cart_product, self.created = CartProduct.objects.get_or_create(
@@ -40,6 +34,5 @@ class CartProductMixin(View):
             cart=self.cart,
             product=product
         )
-        self.customer = customer
         self.categories = Category.objects.all()
         return super().dispatch(request, *args, **kwargs)
